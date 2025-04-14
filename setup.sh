58a8d2f18f23596012280c0d5d269e0f4e353e24
I͍͍̪̖̥͎̺͈̓ͯͨ̄̌̐̍̍ a͊ͦ̇ͤ̾m̴̴̸̵̧̡̛̞͔̮̖͉̝̥͔̟̺̼̟̈́̄̔ͦ̅͐ͦ̑͗ͬͮ́ͯ̓͘ a̓ d̪̜͗̀ͩ̐̽ͫ̃ͮ̽̃͢͞ǒ̘̤͐̇̃͒͝ͅr̋į̸͓̠ͤ́̋̓͊̆͋̄ͧ̒͆̚̕t̡͔͐̏ͅơ̩͍͍̯̮̪̰̯̟̻̙̯̋̊͌͑ͯͤͣ̓̎͗͒́͛̑̚͢ <143878567+trifoil@users.noreply.github.com>
2025-04-14 20:27:25 +0200

    hehe

--- setup.sh
+++ setup.sh
@@ -2,19 +2,40 @@
 
 dnf update -y
 
-sudo dnf install @virtualization -y
+dnf install @virtualization -y
+dnf group install --with-optional virtualization -y
+systemctl start libvirtd 
+systemctl enable libvirtd
+usermod -a -G libvirt $(whoami)
 
-sudo dnf group install --with-optional virtualization -y
+dnf install texlive-scheme-full libreoffice texstudio deluge freecad inkscape blender -y
+dnf install krita -y
+dnf install helvum btop fastfetch conky wine winetricks -y
+sudo dnf install hydrapaper -y
+flatpak install flathub org.videolan.VLC -y
+flatpak install rustdesk -y
+flatpak install zed -y
+flatpak install io.github.shiftey.Desktop -y
+flatpak install flathub com.simulide.simulide -y
+flatpak install flathub cc.arduino.IDE2 -y
+flatpak install flathub org.inkscape.Inkscape -y
+flatpak install flathub com.boxy_svg.BoxySVG -y
+flatpak install flathub org.torproject.torbrowser-launcher -y
+flatpak install flathub org.tenacityaudio.Tenacity -y
+flatpak install flathub nl.hjdskes.gcolor3 -y
+flatpak install flathub io.gitlab.librewolf-community -y
+flatpak install flathub com.github.xournalpp.xournalpp -y
+flatpak install flathub com.mattjakeman.ExtensionManager -y
+flatpak install flathub org.wireshark.Wireshark -y
+flatpak install flathub com.github.unrud.VideoDownloader -y
+flatpak install flathub io.github.vmkspv.netsleuth -y
+flatpak install flathub de.capypara.FieldMonitor -y
+flatpak install flathub com.logseq.Logseq -y
+flatpak install flathub org.gimp.GIMP -y
+flatpak install flathub fr.romainvigier.MetadataCleaner -y
+flatpak install flathub im.nheko.Nheko -y
+flatpak install flathub io.github.bytezz.IPLookup -y
 
-sudo systemctl start libvirtd 
-#sets the libvirtd service to start on system start
-sudo systemctl enable libvirtd
 
-#add current user to virt manager group
-sudo usermod -a -G libvirt $(whoami)
 
-sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
-
-printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h\n" | sudo tee -a /etc/yum.repos.d/vscodium.repo
-
-sudo dnf install codium -y
+dnf remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine -y
